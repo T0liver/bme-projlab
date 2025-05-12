455b432dcc10efc365ec5a3e6161ae2f5ee8dee0
Kovács Bálint <kbalint624@icloud.com>
2025-05-12 11:06:25 +0200

    grafikus interface -- entitasok a terkepen

---
+++ fungorium/src/main/java/bme/EntitasView.java
@@ -0,0 +1,17 @@
+package bme;
+
+import java.awt.image.BufferedImage;
+
+public class EntitasView {
+    private Object entitas;
+    private int mexoX;
+    private int mexoY;
+    private Jatekos jatekos;
+    public BufferedImage kinezet;
+
+    public void draw(GameWindow gw) {
+
+    }
+
+
+}
