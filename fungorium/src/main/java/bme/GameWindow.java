1e78384a139a3b0634c9fc7539771677cbd3cd00
Áron Ujvárosi <aron.ujvarosi@edu.bme.hu>
2025-05-12 11:23:45 +0200

    GameWindow skeleton

--- fungorium/src/main/java/bme/GameWindow.java
+++ fungorium/src/main/java/bme/GameWindow.java
@@ -1,4 +1,33 @@
 package bme;
 
+import java.util.List;
+
 public class GameWindow {
+
+    private int screenWidth;
+    private int screenHeight;
+    private Jatekvezerlo jatekvezerlo;
+    private JatekvezerloView jatekvezerloView;
+    //JatekosView
+    private JatekosMenuView jelenlegiJatekosMenuView;
+    private List<EntitasView> entitasView;
+    private TerkepView terkepView;
+
+    private void renderMenu(){
+
+    }
+
+    private void renderGame(){
+
+    }
+
+    public void drawSprite(EntitasView entitasView){
+
+    }
+
+    public void drawTerkep(){
+
+    }
+
+
 }
---
+++ fungorium/src/main/java/bme/JatekvezerloView.java
@@ -0,0 +1,4 @@
+package bme;
+
+public class JatekvezerloView {
+}
