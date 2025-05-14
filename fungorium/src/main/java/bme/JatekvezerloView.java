306f9c4adc3b982b6caafad8751766e6af7b2991
T0liver <taschnero@edu.bme.hu>
2025-05-14 14:27:36 +0200

    JatékvezérlőView alap dolgok

--- fungorium/src/main/java/bme/JatekvezerloView.java
+++ fungorium/src/main/java/bme/JatekvezerloView.java
@@ -1,9 +1,24 @@
 package bme;
 
 public class JatekvezerloView {
+    /**
+     * a körök lebonyolításáért felelős vezérlő
+     */
+    private Jatekvezerlo jatekvezerlo;
 
+    /**
+     * Publikus konstruktor a view inicializálására
+     * @param jatekvezerlo a körök lebonyolításáért felelős vezérlő
+     */
+    public JatekvezerloView(Jatekvezerlo jatekvezerlo) {
+        this.jatekvezerlo = jatekvezerlo;
+    }
 
-    public JatekvezerloView(Jatekvezerlo jatekvezerlo){
-
+    /**
+     * Publikus getter a játék játékvezérlőjének lekérdezésére.
+     * @return
+     */
+    public Jatekvezerlo getJatekvezerlo() {
+        return jatekvezerlo;
     }
 }
